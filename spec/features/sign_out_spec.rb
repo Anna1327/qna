1404042b245bd75944ed3e7efd5d9d6f35912121
Anna1327 <aoi.yatsu@ya.ru>
2023-11-26 04:59:43 +0300

    add feature test sign_out

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to end session
+  As an authorized User
+  I'd like to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+  background do
+    sign_in(user)
+    click_on I18n.t('main.sign_out')
+  end
+
+  scenario "Authorized user tries to sign out" do
+    expect(page).to have_content I18n.t('devise.sessions.signed_out')
+  end
+end
