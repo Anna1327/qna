58e2753bf22cebd4c1c3d12f7754c37bc6309d55
Anna1327 <aoi.yatsu@ya.ru>
2023-12-14 23:25:22 +0300

    add validation for url link

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -9,9 +9,10 @@ feature 'User can add links to question', %q{
 } do
 
   given(:user) { create :user }
-  given(:gist_url) { 'https://gist.github.com/Anna1327/176c70dc13cd0fd5688fc4da127416c5' }
 
   describe 'Authenticated user', js: true do
+    given(:gist_url) { 'https://gist.github.com/Anna1327/176c70dc13cd0fd5688fc4da127416c5' }
+
     background do
       sign_in(user)
       visit new_question_path
@@ -20,31 +21,47 @@ feature 'User can add links to question', %q{
       fill_in 'Body', with: 'Test body'
 
       click_on I18n.t('links.new.add_link')
-
-      fill_in 'Link name', with: 'My gist'
-      fill_in 'Url', with: gist_url
     end
 
-    scenario 'User can add link when asks question' do
-      click_on 'Ask'
-
-      expect(page).to have_link 'My gist', href: gist_url
-    end
-
-    scenario 'User can add multiple links when asks question' do
-      url = 'https://thinknetica.com/'
-
-      click_on I18n.t('links.new.add_link')
-
-      within all('.nested_fields').last do
-        fill_in 'Link name', with: 'Thinknetica'
-        fill_in 'Url', with: url
+    context 'User can to add link with valid params' do
+      background do
+        fill_in 'Link name', with: 'My gist'
+        fill_in 'Url', with: gist_url
       end
 
-      click_on I18n.t('questions.new.ask')
+      scenario 'add link when asks question' do
+        click_on I18n.t('questions.new.ask')
+        expect(page).to have_link 'My gist', href: gist_url
+      end
 
-      expect(page).to have_link 'My gist', href: gist_url
-      expect(page).to have_link 'Thinknetica', href: url
+      scenario 'add multiple links when asks question' do
+        url = 'https://thinknetica.com/'
+
+        click_on I18n.t('links.new.add_link')
+
+        within all('.nested_fields').last do
+          fill_in 'Link name', with: 'Thinknetica'
+          fill_in 'Url', with: url
+        end
+
+        click_on I18n.t('questions.new.ask')
+
+        expect(page).to have_link 'My gist', href: gist_url
+        expect(page).to have_link 'Thinknetica', href: url
+      end
+
+      context 'User can not to add link with invalid params' do
+        background do
+          fill_in 'Link name', with: 'My gist'
+          fill_in 'Url', with: 'url'
+          click_on I18n.t('questions.new.ask')
+        end
+  
+        scenario 'with invalid url' do
+          expect(page).not_to have_link 'My gist', href: 'url'
+          expect(page).to have_content 'Links url is invalid'
+        end
+      end
     end
   end
 end
