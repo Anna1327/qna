5e55371722c29a018d9e5a24d9aebb84846b4f7e
Anna1327 <aoi.yatsu@ya.ru>
2023-12-14 21:23:55 +0300

    user can add multiple links when asks question

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -11,7 +11,7 @@ feature 'User can add links to question', %q{
   given(:user) { create :user }
   given(:gist_url) { 'https://gist.github.com/Anna1327/176c70dc13cd0fd5688fc4da127416c5' }
 
-  describe 'Authenticated user' do
+  describe 'Authenticated user', js: true do
     background do
       sign_in(user)
 
@@ -22,12 +22,25 @@ feature 'User can add links to question', %q{
       fill_in 'Title', with: 'Test question'
       fill_in 'Body', with: 'Test body'
 
+      click_on "Add link"
+
       fill_in 'Link name', with: 'My gist'
       fill_in 'Url', with: gist_url
 
       click_on 'Ask'
 
       expect(page).to have_link 'My gist', href: gist_url
+    end
+
+    scenario 'User can add multiple links when asks question' do
+      url = 'https://thinknetica.com/'
+
+      click_on "Add link"
+
+      within all('.nested_fields').last do
+        fill_in 'Link name', with: 'Thinknetica'
+        fill_in 'Url', with: url
+      end
     end
   end
 end
