790115830a6baa513d3a1c22436f69b00b5eef82
Anna1327 <aoi.yatsu@ya.ru>
2023-12-21 04:07:12 +0300

    add concern for link model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,12 +1,12 @@
 class Answer < ApplicationRecord
+  include Linkable
+
   has_many_attached :files
-  has_many :links, dependent: :destroy, as: :linkable
   has_one :reward, dependent: :destroy
 
   belongs_to :question
   belongs_to :author, class_name: 'User'
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank
   accepts_nested_attributes_for :reward, reject_if: :all_blank
 
   validates :body, presence: true
