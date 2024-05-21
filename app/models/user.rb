85a6eb2af3d46eb3fdfbe2a800b0458e033b8781
Anna1327 <aoi.yatsu@ya.ru>
2024-05-22 02:04:10 +0300

    add sign in with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:github]
   
+  has_many :authorizations, dependent: :destroy
   has_many :questions, class_name: 'Question', foreign_key: 'author_id', dependent: :destroy
   has_many :answers, class_name: 'Answer', foreign_key: 'author_id', dependent: :destroy
   has_many :rewards, through: :answers
@@ -12,5 +14,25 @@ class User < ApplicationRecord
 
   def author_of?(resource)
     self.id == resource.author_id
+  end
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+    user
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid.to_s)
   end
 end
