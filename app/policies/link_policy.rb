613c290ec8d5b81897ba9e368bc4f46568795325
Anna1327 <aoi.yatsu@ya.ru>
2024-07-09 07:14:26 +0300

    add link policy

---
+++ app/policies/link_policy.rb
@@ -0,0 +1,11 @@
+class LinkPolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def destroy?
+    user&.author_of?(record.linkable)
+  end
+end
