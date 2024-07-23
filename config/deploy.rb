130ba932c00aebc6a2e28090e2d5c4645c1b7b29
Anna1327 <aoi.yatsu@ya.ru>
2024-07-23 17:54:03 +0300

    add config deploy

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.19.1"
+nvm use 18
+
+set :application, "qna"
+set :repo_url, "git@github.com:Anna1327/qna.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+set :node_env, 'production'
+set :node_ver, '18.20.4'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", 'config/master.key'
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
+
+desc "tail the application logfile"
+task :log do
+  log = "#{application_dir}/current/log/#{node_env}.log"
+  run "tail -f #{log}"
+end
