5d0e6af1effb9438146261fc8fab29c87c93264c
Anna1327 <aoi.yatsu@ya.ru>
2024-07-23 20:33:13 +0300

    fix webpack config

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -2,29 +2,9 @@ const Handlebars = require("handlebars");
 
 module.exports = {
   module: {
-    rules: [
-      {
-        test: /\.hbs$/i,
-        loader: "html-loader",
-        options: {
-          preprocessor: async (content, loaderContext) => {
-            let result;
-
-            try {
-              result = await Handlebars.compile(content)({
-                firstname: "Value",
-                lastname: "OtherValue",
-              });
-            } catch (error) {
-              await loaderContext.emitError(error);
-
-              return content;
-            }
-
-            return result;
-          },
-        },
-      },
-    ],
+    rules: [{ test: /\.hbs$/, use: "html-loader" }],
   },
+  plugins: [
+    new Handlebars({ template: "app/javascript/templates/answer.hbs" }),
+  ],
 };
