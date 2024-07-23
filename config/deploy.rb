987c34b4b662f1692cab23805e2fcbb7243475ff
Anna1327 <aoi.yatsu@ya.ru>
2024-07-23 19:20:24 +0300

    fix deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,17 +7,9 @@ set :repo_url, "git@github.com:Anna1327/qna.git"
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deployer/qna"
 set :deploy_user, 'deployer'
-set :node_env, 'production'
-set :node_ver, '18.20.4'
 
 # Default value for :linked_files is []
 append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
-
-desc "tail the application logfile"
-task :log do
-  log = "#{application_dir}/current/log/#{node_env}.log"
-  run "tail -f #{log}"
-end
