56c559f2d4afc63e47e107ff14501dc0e6183c2c
Anna1327 <aoi.yatsu@ya.ru>
2024-07-26 02:34:45 +0300

    fix package json

--- config/webpack/loaders/handlebars.js
+++ config/webpack/loaders/handlebars.js
@@ -1,9 +1,9 @@
-import Handlebars from "handlebars/runtime";
-
-Handlebars.registerHelper("author_of?", function (author_id) {
-  return author_id === gon.current_user_id;
-});
-
-Handlebars.registerHelper("can_vote?", function (author_id) {
-  return gon.current_user_id && author_id !== gon.current_user_id;
-});
+module.exports = {
+  test: /\.hbs$/,
+  loader: "handlebars-loader",
+  options: {
+    precompileOptions: {
+      knownHelpersOnly: false,
+    },
+  },
+};
