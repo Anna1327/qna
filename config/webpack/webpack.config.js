988f595a8ba91903bf9743d1d9b2c131df63e734
Anna1327 <aoi.yatsu@ya.ru>
2024-07-26 02:19:56 +0300

    fix

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   module: {
-    rules: [{ test: /\.hbs$/, use: "html-loader" }],
+    rules: [{ test: /\.hbs$/, use: "handlebars-loader" }],
   },
   plugins: [
     new Handlebars({ template: "app/javascript/templates/answer.hbs" }),
