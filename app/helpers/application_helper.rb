a1ede51be8991f94fa7cc07eff043ccd3f7d5768
Anna1327 <aoi.yatsu@ya.ru>
2024-09-02 07:11:15 +0300

    update helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  FLASH_TO_CSS = {
+    notice: 'alert-success',
+    error: 'alert-danger',
+    alert: 'alert-warning',
+    success: 'alert-success'
+  }.freeze
+
+  def flash_to_css(key)
+    FLASH_TO_CSS.fetch(key.to_sym, key)
+  end
+
+  def collection_cache_key_for(model)
+    klass = model.to_s.capitalize.constantize
+    count = klass.count
+    max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s)
+    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
+  end
 end
